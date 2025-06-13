734a2a166ce9e7b8075ce3ec1ade2205cd7cc191
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-14 01:36:26 +0900

    react-router-data-mode

--- shadcnV4/src/routes.tsx
+++ shadcnV4/src/routes.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
-//Declarative Mode
-export default function RouteList() {
+import Product from "./pages/Product";
+import Details from "./pages/Details";
+import Dashboard from "./pages/Dashboard";
+
+export const RouteList = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Dashboard />}>
+            <Route index element={<Product />} />
+            <Route path="detail" element={<Details />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
