45e459a0ed76690e785feaa24e9a36ad6ec7d054
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-16 00:10:51 +0900

    Data Route

--- shadcnV4/src/data-routes.tsx
+++ shadcnV4/src/data-routes.tsx
@@ -1,8 +1,25 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Product from "./pages/Product";
+import Details from "./pages/Details";
+import Dashboard from "./pages/Dashboard";
+import ProductLayouts from "./components/ui/ProductLayouts";
 
 export const router = createBrowserRouter([
   { path: "/", Component: Home },
-  { path: "/about", Component: About },
+  { path: "/about/*", Component: About },
+  {
+    Component: ProductLayouts,
+    children: [
+      {
+        path: "products",
+        Component: Dashboard,
+        children: [
+          { index: true, Component: Product },
+          { path: ":pid/edit/:uid?", Component: Details },
+        ],
+      },
+    ],
+  },
 ]);
