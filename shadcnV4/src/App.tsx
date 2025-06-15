d30e0661ea874f76746659809bdb613e408c2494
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-16 01:31:43 +0900

    Simple Navigation added

--- shadcnV4/src/App.tsx
+++ shadcnV4/src/App.tsx
@@ -1,13 +1,25 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Home from "@/pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/404";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </>
   );
 }
