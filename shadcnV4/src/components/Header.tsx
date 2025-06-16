917bb1620f84d0fc9a779068705e520b49503661
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-17 00:34:42 +0900

    Nav list for mobile

--- shadcnV4/src/components/Header.tsx
+++ shadcnV4/src/components/Header.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleFunction = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="bg-sky-400 p-4 text-white">
       {/* container mx-auto ;width auto
@@ -10,7 +16,12 @@ function Header() {
         <Link to="/" className="text-xl font-bold">
           Fashion Shop
         </Link>
-        <button className="block text-xl text-white lg:hidden">&#8801;</button>
+        <button
+          className="block text-xl text-white lg:hidden"
+          onClick={toggleFunction}
+        >
+          &#8801;
+        </button>
         {/*Nav for Desktop view  */}
         <ul className="hidden space-x-6 lg:flex">
           <li>
@@ -44,6 +55,14 @@ function Header() {
             </NavLink>
           </li>
         </ul>
+        {/*Mobile View Navigation*/}
+        <div className="">
+          <div className="">
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/cart">Cart</Link>
+          </div>
+        </div>
       </nav>
     </header>
   );
