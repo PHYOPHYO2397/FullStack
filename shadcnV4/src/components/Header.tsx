f39c7a127966d053138fa12ab9b1b3c90b7f5e48
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-16 23:43:04 +0900

    Nav list for mobile phone

--- shadcnV4/src/components/Header.tsx
+++ shadcnV4/src/components/Header.tsx
@@ -2,12 +2,15 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <header className="">
-      <nav className="">
-        <Link to="/" className="">
+    <header className="bg-sky-400 p-4 text-white">
+      {/* container mx-auto ;width auto
+      middle justify between ,  */}
+
+      <nav className="container mx-auto flex items-center justify-between">
+        <Link to="/" className="text-xl font-bold">
           Fashion Shop
         </Link>
-        <button className=""> &#8801;</button>
+        <button className="block text-xl lg:hidden"> &#8801;</button>
       </nav>
     </header>
   );
