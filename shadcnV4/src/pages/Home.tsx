31f7a81014f6a8e0e2d6435e662552553ee2ba34
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-19 00:49:56 +0900

    Home Screen Added

--- shadcnV4/src/pages/Home.tsx
+++ shadcnV4/src/pages/Home.tsx
@@ -3,15 +3,20 @@ import { Link } from "react-router";
 function Home() {
   return (
     <section className="flex grow items-center justify-center">
-      <div className="text-center">
+      <div className="container mx-auto text-center md:text-left">
         <h1 className="mb-4 text-3xl font-bold md:text-5xl">
-          Welcome to our Fashion Shop
+          Welcome to Our Fashion Shop
         </h1>
         <p className="mb-8 text-lg text-gray-700 md:text-xl">
           This is simple landing page built with React,Typescript and Tailwind
           CSS
         </p>
-        <Link to={"/shop"}>Get Started</Link>
+        <Link
+          to={"/shop"}
+          className="rounded-4xl bg-sky-500 px-6 py-3 text-white shadow hover:bg-sky-300"
+        >
+          Get Started
+        </Link>
       </div>
     </section>
   );
