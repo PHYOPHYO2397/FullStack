cd1a7c4bdda132f87764861dc86deb70f4bb778a
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-21 23:44:44 +0900

    install extra dependency

--- shadcnV4/src/App.tsx
+++ shadcnV4/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Home from "@/pages/Home";
