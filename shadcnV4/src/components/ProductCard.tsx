f6d1661302a35599fb37c92777879be40b49e1fd
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-06-24 23:26:31 +0900

    zustand

--- shadcnV4/src/components/ProductCard.tsx
+++ shadcnV4/src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 //can use type keyword instead of interface
 interface productType {
   //   id: number;
@@ -10,22 +11,32 @@ interface productType {
 //FC means functional component
 //const ProductCard: React.FC<productType> = ({ id, name, price, img }) => {
 const ProductCard = ({ name, price, img }: productType) => {
+  const [imgLoad, setImgLoad] = useState(false);
+  const handleImageLoad = () => {
+    setImgLoad(true);
+  };
+  console.log(imgLoad);
   return (
     <>
       <div className="rounded-lg bg-white p-4 shadow-lg">
         <div className="h-48 w-full">
+          {!imgLoad && <div className="animate-pulse"></div>}
           <img
             src={img}
             alt="name"
             className="h-full w-full rounded-md object-contain"
+            onLoad={handleImageLoad}
           />
         </div>
-        <div className="flex justify-between">
+        <div className="mt-4 flex justify-between">
           <div>
-            <h3 className="">{name}</h3>
-            <p className="">${price}</p>
+            <h3 className="text-lg font-bold">{name}</h3>
+            <p className="text-gray-400">${price}</p>
           </div>
           <Button>Add to Cart</Button>
+          {/* <button className="rounded-md bg-orange-400 px-4 py-2 text-white hover:bg-sky-300">
+            Add to Cart
+          </button> */}
         </div>
       </div>
     </>
---
+++ shadcnV4/src/store/cartStore.ts
@@ -0,0 +1,3 @@
+interface cartItems {
+  id: number;
+}
